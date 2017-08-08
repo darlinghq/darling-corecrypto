5eb832642f7cf07386324ad6f92982653b07ea0e
Lubos Dolezel <lubos@dolezel.info>
2017-08-08 16:45:08 +0200

    Implement ccsha224 and ccsha256

--- src/ccsha3xx.c
+++ src/ccsha3xx.c
@@ -1,11 +1,5 @@
 #include <corecrypto/ccsha2.h>
 
-const struct ccdigest_info *ccsha224_di(void) {
-}
-
-const struct ccdigest_info *ccsha256_di(void) {
-}
-
 const struct ccdigest_info *ccsha384_di(void) {
 }
 
