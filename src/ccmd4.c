d055f73ea001044db286e56d9d1ffdd76a8ea7b7
Lubos Dolezel <lubos@dolezel.info>
2020-02-15 12:56:42 +0100

    Fix MD5 and MD4 implementations (darlinghq/darling#641)

--- src/ccmd4.c
+++ src/ccmd4.c
@@ -19,55 +19,26 @@
 
 #include <corecrypto/ccdigest.h>
 #include <corecrypto/ccmd4.h>
+#include <corecrypto/cc_priv.h>
+#include <corecrypto/ccdigest_priv.h>
 #include <string.h>
 
 #define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
 
-static void md4_final(const struct ccdigest_info* di, ccdigest_ctx_t ctx, unsigned char* out);
 static void md4_compress(ccdigest_state_t state, size_t nblocks, const void* in);
 
-static const uint32_t md4_initial_state[4+18] = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 };
+const uint32_t ccmd4_initial_state[4] = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 };
 
 const struct ccdigest_info ccmd4_ltc_di = {
-		.initial_state = md4_initial_state,
+		.initial_state = ccmd4_initial_state,
 		.compress = md4_compress,
-		.final = md4_final,
+		.final = ccdigest_final_64le,
 		.output_size = CCMD4_OUTPUT_SIZE,
 		.state_size = CCMD4_STATE_SIZE,
 		.block_size = CCMD4_BLOCK_SIZE,
 		.oid = CC_DIGEST_OID_MD4,
 		.oid_size = 10,
 };
-
-// Taken from RFC1320
-
-/* POINTER defines a generic pointer type */
-typedef unsigned char *POINTER;
-
-/* UINT2 defines a two byte word */
-typedef unsigned short int UINT2;
-
-/* UINT4 defines a four byte word */
-typedef unsigned int UINT4;
-
-struct MD4_CTX {
-	UINT4 state[4];                                   /* state (ABCD) */
-	UINT4 count[2];        /* number of bits, modulo 2^64 (lsb first) */
-	unsigned char buffer[64];                         /* input buffer */
-};
-
-#define FF(a, b, c, d, x, s) { \
-    (a) += F ((b), (c), (d)) + (x); \
-    (a) = ROTATE_LEFT ((a), (s)); \
-  }
-#define GG(a, b, c, d, x, s) { \
-    (a) += G ((b), (c), (d)) + (x) + (UINT4)0x5a827999; \
-    (a) = ROTATE_LEFT ((a), (s)); \
-  }
-#define HH(a, b, c, d, x, s) { \
-    (a) += H ((b), (c), (d)) + (x) + (UINT4)0x6ed9eba1; \
-    (a) = ROTATE_LEFT ((a), (s)); \
-  }
 
 #define S11 3
 #define S12 7
@@ -82,196 +53,114 @@ struct MD4_CTX {
 #define S33 11
 #define S34 15
 
+// Taken from RFC1320
+typedef uint32_t UINT4;
+
+/* ROTATE_LEFT rotates x left n bits.
+ */
+#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
+
+/* FF, GG and HH are transformations for rounds 1, 2 and 3 */
+/* Rotation is separate from addition to prevent recomputation */
+
+#define FF(a, b, c, d, x, s) { \
+    (a) += F ((b), (c), (d)) + (x); \
+    (a) = ROTATE_LEFT ((a), (s)); \
+  }
+#define GG(a, b, c, d, x, s) { \
+    (a) += G ((b), (c), (d)) + (x) + 0x5a827999; \
+    (a) = ROTATE_LEFT ((a), (s)); \
+  }
+#define HH(a, b, c, d, x, s) { \
+    (a) += H ((b), (c), (d)) + (x) + 0x6ed9eba1; \
+    (a) = ROTATE_LEFT ((a), (s)); \
+  }
+
 #define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
-#define G(x, y, z) (((x) & (y)) | ((x) & (z)) | ((y) & (z)))
+#define G(x, y, z) (((x) & (z)) | ((y) & (~z)))
 #define H(x, y, z) ((x) ^ (y) ^ (z))
+#define I(x, y, z) ((y) ^ ((x) | (~z)))
 
-static void Decode (UINT4 *output, const unsigned char *input, unsigned int len)
+static void md4_compress(ccdigest_state_t state, unsigned long nblocks, const void *in)
 {
-	unsigned int i, j;
+    uint32_t x[16];
+	uint32_t a, b, c, d;
+    uint32_t* s = ccdigest_u32(state);
+    const uint8_t* buf = in;
 
-	for (i = 0, j = 0; j < len; i++, j += 4)
-		output[i] = ((UINT4)input[j]) | (((UINT4)input[j+1]) << 8) |
-		(((UINT4)input[j+2]) << 16) | (((UINT4)input[j+3]) << 24);
-}
-
-static void Encode (unsigned char *output, UINT4 *input, unsigned int len)
-{
-	unsigned int i, j;
-
-	for (i = 0, j = 0; j < len; i++, j += 4) {
-		output[j] = (unsigned char)(input[i] & 0xff);
-		output[j+1] = (unsigned char)((input[i] >> 8) & 0xff);
-		output[j+2] = (unsigned char)((input[i] >> 16) & 0xff);
-		output[j+3] = (unsigned char)((input[i] >> 24) & 0xff);
-	}
-}
-
-static void MD4Transform (UINT4 state[4], const unsigned char block[64])
-{
-	UINT4 a = state[0], b = state[1], c = state[2], d = state[3], x[16];
-
-	Decode (x, block, 64);
-
-	/* Round 1 */
-	FF (a, b, c, d, x[ 0], S11); /* 1 */
-	FF (d, a, b, c, x[ 1], S12); /* 2 */
-	FF (c, d, a, b, x[ 2], S13); /* 3 */
-	FF (b, c, d, a, x[ 3], S14); /* 4 */
-	FF (a, b, c, d, x[ 4], S11); /* 5 */
-	FF (d, a, b, c, x[ 5], S12); /* 6 */
-	FF (c, d, a, b, x[ 6], S13); /* 7 */
-	FF (b, c, d, a, x[ 7], S14); /* 8 */
-	FF (a, b, c, d, x[ 8], S11); /* 9 */
-	FF (d, a, b, c, x[ 9], S12); /* 10 */
-	FF (c, d, a, b, x[10], S13); /* 11 */
-	FF (b, c, d, a, x[11], S14); /* 12 */
-	FF (a, b, c, d, x[12], S11); /* 13 */
-	FF (d, a, b, c, x[13], S12); /* 14 */
-	FF (c, d, a, b, x[14], S13); /* 15 */
-	FF (b, c, d, a, x[15], S14); /* 16 */
-
-	/* Round 2 */
-	GG (a, b, c, d, x[ 0], S21); /* 17 */
-	GG (d, a, b, c, x[ 4], S22); /* 18 */
-	GG (c, d, a, b, x[ 8], S23); /* 19 */
-	GG (b, c, d, a, x[12], S24); /* 20 */
-	GG (a, b, c, d, x[ 1], S21); /* 21 */
-	GG (d, a, b, c, x[ 5], S22); /* 22 */
-	GG (c, d, a, b, x[ 9], S23); /* 23 */
-	GG (b, c, d, a, x[13], S24); /* 24 */
-	GG (a, b, c, d, x[ 2], S21); /* 25 */
-	GG (d, a, b, c, x[ 6], S22); /* 26 */
-	GG (c, d, a, b, x[10], S23); /* 27 */
-	GG (b, c, d, a, x[14], S24); /* 28 */
-	GG (a, b, c, d, x[ 3], S21); /* 29 */
-	GG (d, a, b, c, x[ 7], S22); /* 30 */
-	GG (c, d, a, b, x[11], S23); /* 31 */
-	GG (b, c, d, a, x[15], S24); /* 32 */
-
-	/* Round 3 */
-	HH (a, b, c, d, x[ 0], S31); /* 33 */
-	HH (d, a, b, c, x[ 8], S32); /* 34 */
-	HH (c, d, a, b, x[ 4], S33); /* 35 */
-	HH (b, c, d, a, x[12], S34); /* 36 */
-	HH (a, b, c, d, x[ 2], S31); /* 37 */
-	HH (d, a, b, c, x[10], S32); /* 38 */
-	HH (c, d, a, b, x[ 6], S33); /* 39 */
-	HH (b, c, d, a, x[14], S34); /* 40 */
-	HH (a, b, c, d, x[ 1], S31); /* 41 */
-	HH (d, a, b, c, x[ 9], S32); /* 42 */
-	HH (c, d, a, b, x[ 5], S33); /* 43 */
-	HH (b, c, d, a, x[13], S34); /* 44 */
-	HH (a, b, c, d, x[ 3], S31); /* 45 */
-	HH (d, a, b, c, x[11], S32); /* 46 */
-	HH (c, d, a, b, x[ 7], S33); /* 47 */
-	HH (b, c, d, a, x[15], S34); /* 48 */
-
-	state[0] += a;
-	state[1] += b;
-	state[2] += c;
-	state[3] += d;
-
-	/* Zeroize sensitive information.
-	 */
-	memset ((POINTER)x, 0, sizeof (x));
-}
-
-static const unsigned char PADDING[64] = {
-		0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-};
-
-static void MD4Update (struct MD4_CTX *context, const unsigned char *input, unsigned int inputLen)
-{
-	unsigned int i, index, partLen;
-
-	/* Compute number of bytes mod 64 */
-	index = (unsigned int)((context->count[0] >> 3) & 0x3F);
-	/* Update number of bits */
-	if ((context->count[0] += ((UINT4)inputLen << 3)) < ((UINT4)inputLen << 3))
-		context->count[1]++;
-
-	context->count[1] += ((UINT4)inputLen >> 29);
-
-	partLen = 64 - index;
-	/* Transform as many times as possible.
-	 */
-	if (inputLen >= partLen) {
-		memcpy
-				((POINTER)&context->buffer[index], (POINTER)input, partLen);
-		MD4Transform (context->state, context->buffer);
-
-		for (i = partLen; i + 63 < inputLen; i += 64)
-			MD4Transform (context->state, &input[i]);
-
-		index = 0;
-	}
-	else
-		i = 0;
-
-	/* Buffer remaining input */
-	memcpy
-			((POINTER)&context->buffer[index], (POINTER)&input[i],
-			 inputLen-i);
-}
-
-static void MD4Final (unsigned char digest[16], struct MD4_CTX *context)
-{
-	unsigned char bits[8];
-	unsigned int index, padLen;
-
-	/* Save number of bits */
-	Encode (bits, context
-	->count, 8);
-
-	/* Pad out to 56 mod 64.
-	 */
-	index = (unsigned int) ((context->count[0] >> 3) & 0x3f);
-	padLen = (index < 56) ? (56 - index) : (120 - index);
-	MD4Update (context, PADDING, padLen
-	);
-
-	/* Append length (before padding) */
-	MD4Update (context, bits,
-	8);
-	/* Store state in digest */
-	Encode (digest, context
-	->state, 16);
-
-	/* Zeroize sensitive information.
-	 */
-	memset ((POINTER)
-	context, 0, sizeof (*context));
-}
-
-static void md4_final(const struct ccdigest_info* di, ccdigest_ctx_t ctx, unsigned char* out)
-{
-	struct MD4_CTX* mdctx = (struct MD4_CTX*) ccdigest_u8(ccdigest_state(di, ctx));
-	struct ccdigest_state* state = ccdigest_state(di, ctx);
-
-	// Push the remaining data
-	MD4Update(mdctx, ccdigest_data(di, ctx), ccdigest_num(di, ctx));
-	MD4Final(out, mdctx);
-}
-
-static void md4_compress(ccdigest_state_t state, size_t nblocks, const void* in)
-{
-	struct MD4_CTX* ctx = (struct MD4_CTX*) ccdigest_u8(state);
-	unsigned int i;
-	const unsigned char* pos = (const unsigned char*) in;
-
-	for (i = 0; i < nblocks; i++)
+    for (size_t block = 0; block < nblocks; block++)
 	{
-		MD4Update(ctx, pos, CCMD4_BLOCK_SIZE);
-		pos += CCMD4_BLOCK_SIZE;
-	}
-}
+        /* copy state */
+        a = s[0];
+        b = s[1];
+        c = s[2];
+        d = s[3];
 
-const uint32_t ccmd4_initial_state[4] = {
-    0x00000000,
-    0x00000000,
-    0x00000000,
-    0x00000000,
-};
+        /* copy the state into 512-bits into W[0..15] */
+        for (int i = 0; i < 16; i++)
+            CC_LOAD32_LE(x[i], buf + (4*i));
+
+        /* Round 1 */
+        FF (a, b, c, d, x[ 0], S11); /* 1 */
+        FF (d, a, b, c, x[ 1], S12); /* 2 */
+        FF (c, d, a, b, x[ 2], S13); /* 3 */
+        FF (b, c, d, a, x[ 3], S14); /* 4 */
+        FF (a, b, c, d, x[ 4], S11); /* 5 */
+        FF (d, a, b, c, x[ 5], S12); /* 6 */
+        FF (c, d, a, b, x[ 6], S13); /* 7 */
+        FF (b, c, d, a, x[ 7], S14); /* 8 */
+        FF (a, b, c, d, x[ 8], S11); /* 9 */
+        FF (d, a, b, c, x[ 9], S12); /* 10 */
+        FF (c, d, a, b, x[10], S13); /* 11 */
+        FF (b, c, d, a, x[11], S14); /* 12 */
+        FF (a, b, c, d, x[12], S11); /* 13 */
+        FF (d, a, b, c, x[13], S12); /* 14 */
+        FF (c, d, a, b, x[14], S13); /* 15 */
+        FF (b, c, d, a, x[15], S14); /* 16 */
+
+        /* Round 2 */
+        GG (a, b, c, d, x[ 0], S21); /* 17 */
+        GG (d, a, b, c, x[ 4], S22); /* 18 */
+        GG (c, d, a, b, x[ 8], S23); /* 19 */
+        GG (b, c, d, a, x[12], S24); /* 20 */
+        GG (a, b, c, d, x[ 1], S21); /* 21 */
+        GG (d, a, b, c, x[ 5], S22); /* 22 */
+        GG (c, d, a, b, x[ 9], S23); /* 23 */
+        GG (b, c, d, a, x[13], S24); /* 24 */
+        GG (a, b, c, d, x[ 2], S21); /* 25 */
+        GG (d, a, b, c, x[ 6], S22); /* 26 */
+        GG (c, d, a, b, x[10], S23); /* 27 */
+        GG (b, c, d, a, x[14], S24); /* 28 */
+        GG (a, b, c, d, x[ 3], S21); /* 29 */
+        GG (d, a, b, c, x[ 7], S22); /* 30 */
+        GG (c, d, a, b, x[11], S23); /* 31 */
+        GG (b, c, d, a, x[15], S24); /* 32 */
+
+        /* Round 3 */
+        HH (a, b, c, d, x[ 0], S31); /* 33 */
+        HH (d, a, b, c, x[ 8], S32); /* 34 */
+        HH (c, d, a, b, x[ 4], S33); /* 35 */
+        HH (b, c, d, a, x[12], S34); /* 36 */
+        HH (a, b, c, d, x[ 2], S31); /* 37 */
+        HH (d, a, b, c, x[10], S32); /* 38 */
+        HH (c, d, a, b, x[ 6], S33); /* 39 */
+        HH (b, c, d, a, x[14], S34); /* 40 */
+        HH (a, b, c, d, x[ 1], S31); /* 41 */
+        HH (d, a, b, c, x[ 9], S32); /* 42 */
+        HH (c, d, a, b, x[ 5], S33); /* 43 */
+        HH (b, c, d, a, x[13], S34); /* 44 */
+        HH (a, b, c, d, x[ 3], S31); /* 45 */
+        HH (d, a, b, c, x[11], S32); /* 46 */
+        HH (c, d, a, b, x[ 7], S33); /* 47 */
+        HH (b, c, d, a, x[15], S34); /* 48 */
+
+
+        /* Update our state */
+        s[0] = s[0] + a;
+        s[1] = s[1] + b;
+        s[2] = s[2] + c;
+        s[3] = s[3] + d;
+
+        buf += CCMD4_BLOCK_SIZE;
+    }
+}
