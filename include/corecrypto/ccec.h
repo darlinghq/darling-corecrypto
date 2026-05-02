darlinghq/darling-corecrypto

commit c3013bb1cee467ceb8c2c2951af1fc6073cd631c

Add the ability to fully disable transparent unions in CoreCrypto headers CoreCrypto itself will still be built using transparent unions, but this change allows external code using CoreCrypto to disable transparent unions in the API
This is necessary for the updated version of CoreTLS that *seems* like it should work with transparent unions but I guess Clang is choking on them. The workaround for it is to disable transparent unions in the CoreCrypto API (and thus we need this change)

include/corecrypto/ccec.h
@@ -11,6 +11,7 @@ cc_aligned_struct(8) ccec_projective_point;
 
 // Structs/unions
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     struct ccec_point_hdr *hdr;
     ccec_projective_point *_p;
@@ -20,6 +21,20 @@ typedef union {
     const struct cczp   *zp;
     const struct cczp_prime *prime;
 } __attribute__((transparent_union)) ccec_const_cp_t;
+
+#define CCEC_PROJECTIVE_POINT_T_HDR(point) ((point).hdr)
+
+#define CCEC_CONST_CP_T_ZP(cp) ((cp).zp)
+#define CCEC_CONST_CP_T_PRIME(cp) ((cp).prime)
+#else
+typedef ccec_projective_point* ccec_projective_point_t;
+typedef const struct cczp* ccec_const_cp_t;
+
+#define CCEC_PROJECTIVE_POINT_T_HDR(point) ((struct ccec_point_hdr*)(point))
+
+#define CCEC_CONST_CP_T_ZP(cp) ((const struct cczp*)(cp))
+#define CCEC_CONST_CP_T_PRIME(cp) ((const struct cczp_prime*)(cp))
+#endif
 
 struct ccec_ctx_header {
     ccec_const_cp_t      cp;
@@ -35,6 +50,7 @@ typedef struct ccec_full_ctx {
     struct ccec_ctx_header  hdr;
 } __attribute__((aligned(16))) ccec_full_ctx;
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     ccec_full_ctx *_full;
     struct ccec_ctx_header *hdr;
@@ -42,10 +58,22 @@ typedef union {
     struct ccec_ctx_public *pub;
 } __attribute__((transparent_union)) ccec_full_ctx_t;
 
+#define CCEC_FULL_CTX_HDR(ctx) ((ctx).hdr)
+#define CCEC_FULL_CTX_BODY(ctx) ((ctx).body)
+#define CCEC_FULL_CTX_PUB(ctx) ((ctx).pub)
+#else
+typedef ccec_full_ctx* ccec_full_ctx_t;
+
+#define CCEC_FULL_CTX_HDR(ctx) ((struct ccec_ctx_header*)(ctx))
+#define CCEC_FULL_CTX_BODY(ctx) ((struct ccec_ctx_body*)(ctx))
+#define CCEC_FULL_CTX_PUB(ctx) ((struct ccec_ctx_public*)(ctx))
+#endif
+
 typedef struct ccec_pub_ctx {
     struct ccec_ctx_header  hdr;
 } __attribute__((aligned(16))) ccec_pub_ctx;
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     ccec_pub_ctx *_pub;
     ccec_full_ctx *_full;
@@ -55,6 +83,17 @@ typedef union {
     ccec_full_ctx_t fullt;
 } __attribute__((transparent_union)) ccec_pub_ctx_t;
 
+#define CCEC_PUB_CTX_HDR(ctx) ((ctx).hdr)
+#define CCEC_PUB_CTX_BODY(ctx) ((ctx).body)
+#define CCEC_PUB_CTX_PUB(ctx) ((ctx).pub)
+#else
+typedef ccec_pub_ctx* ccec_pub_ctx_t;
+
+#define CCEC_PUB_CTX_HDR(ctx) ((struct ccec_ctx_header*)(ctx))
+#define CCEC_PUB_CTX_BODY(ctx) ((struct ccec_ctx_body*)(ctx))
+#define CCEC_PUB_CTX_PUB(ctx) ((struct ccec_ctx_public*)(ctx))
+#endif
+
 struct ccec_ctx_body {
     struct ccec_ctx_header  hdr;
     cc_unit              ccn[];
@@ -63,7 +102,7 @@ struct ccec_ctx_body {
 // Functions needed by the following macros
 CC_CONST CC_INLINE
 cc_size ccec_cp_n(ccec_const_cp_t cp) {
-    return cp.zp->n;
+    return CCEC_CONST_CP_T_ZP(cp)->n;
 }
 
 CC_CONST CC_INLINE
@@ -75,8 +114,8 @@ size_t ccec_compact_import_pub_size(size_t in_len);
 
 // Macros
 #define ccec_pub_ctx_size(_size_)   (sizeof(struct ccec_ctx_header) + 3 * (_size_))
-#define ccec_ctx_cp(KEY)     (((ccec_pub_ctx_t)(KEY)).hdr->cp)
-#define ccec_cp_prime_bitlen(CP) (ccn_bitlen((CP).zp->n, (CP).prime->ccn))
+#define ccec_ctx_cp(KEY)     (CCEC_PUB_CTX_HDR((ccec_pub_ctx_t)(KEY))->cp)
+#define ccec_cp_prime_bitlen(CP) (ccn_bitlen(CCEC_CONST_CP_T_ZP(CP)->n, CCEC_CONST_CP_T_PRIME(CP)->ccn))
 #define ccec_ctx_bitlen(KEY) (ccec_cp_prime_bitlen(ccec_ctx_cp(KEY)))
 #define ccec_full_ctx_size(_size_)  (sizeof(struct ccec_ctx_header) + 4 * (_size_))
 #define ccec_full_ctx_decl(_size_, _name_)  cc_ctx_decl(ccec_full_ctx, ccec_full_ctx_size(_size_), _name_)
@@ -85,16 +124,18 @@ size_t ccec_compact_import_pub_size(size_t in_len);
 #define ccec_pub_ctx_decl_cp(_cp_, _name_)  ccec_pub_ctx_decl(ccec_ccn_size(_cp_), _name_)
 #define ccec_full_ctx_clear(_size_, _name_) cc_clear(ccec_full_ctx_size(_size_), _name_)
 #define ccec_full_ctx_clear_cp(_cp_, _name_) ccec_full_ctx_clear(ccec_ccn_size(_cp_), _name_)
-#define ccec_ctx_n(KEY)      (((ccec_pub_ctx_t)(KEY)).hdr->cp.zp->n)
+#define ccec_ctx_n(KEY)      (CCEC_CONST_CP_T_ZP(CCEC_PUB_CTX_HDR((ccec_pub_ctx_t)(KEY))->cp)->n)
 #define ccec_cp_prime_size(CP) ((ccec_cp_prime_bitlen(CP)+7)/8)
 #define ccec_ctx_size(KEY) (ccec_cp_prime_size(ccec_ctx_cp(KEY)))
 
-#define ccec_ctx_x(KEY)  (((ccec_pub_ctx_t)(KEY)).body->ccn)
-#define ccec_ctx_y(KEY)  (((ccec_pub_ctx_t)(KEY)).body->ccn + 1 * ccec_ctx_n(KEY))
-#define ccec_ctx_z(KEY)  (((ccec_pub_ctx_t)(KEY)).body->ccn + 2 * ccec_ctx_n(KEY))
-#define ccec_ctx_k(KEY)      (((ccec_full_ctx_t)(KEY)).body->ccn + 3 * ccec_ctx_n(KEY))
+#define ccec_ctx_x(KEY)  (CCEC_PUB_CTX_BODY((ccec_pub_ctx_t)(KEY))->ccn)
+#define ccec_ctx_y(KEY)  (CCEC_PUB_CTX_BODY((ccec_pub_ctx_t)(KEY))->ccn + 1 * ccec_ctx_n(KEY))
+#define ccec_ctx_z(KEY)  (CCEC_PUB_CTX_BODY((ccec_pub_ctx_t)(KEY))->ccn + 2 * ccec_ctx_n(KEY))
+#define ccec_ctx_k(KEY)      (CCEC_FULL_CTX_BODY((ccec_full_ctx_t)(KEY))->ccn + 3 * ccec_ctx_n(KEY))
 
-#define ccec_ctx_point(KEY)    ((ccec_projective_point_t)(((ccec_pub_ctx_t)(KEY)).pub->point))
+#define ccec_ctx_point(KEY)    ((ccec_projective_point_t)(CCEC_PUB_CTX_PUB((ccec_pub_ctx_t)(KEY))->point))
+
+#define ccec_ctx_pub(KEY) ((ccec_pub_ctx_t)(KEY))
 
 // Functions
 CC_INLINE CC_CONST CC_NONNULL_TU((2))
@@ -235,7 +276,7 @@ const ccec_const_cp_t ccec_curve_for_length_lookup(size_t keylen, ...);
 
 CC_INLINE
 void ccec_ctx_init(ccec_const_cp_t cp, ccec_pub_ctx_t key) {
-    key.hdr->cp = cp;
+    CCEC_PUB_CTX_HDR(key)->cp = cp;
 }
 
 size_t ccec_compact_import_pub_size(size_t in_len);

include/corecrypto/ccdh.h
@@ -17,6 +17,7 @@ cc_aligned_struct(16) ccdh_gp;
 
 // Unions/structs
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     cczp_t  zp;
     ccdh_gp *gp;   
@@ -27,6 +28,22 @@ typedef union {
     const ccdh_gp  *gp;       
     ccdh_gp_t      _ncgp;
 } __attribute__((transparent_union)) ccdh_const_gp_t;
+
+#define CCDH_GP_T_ZP(gp) ((gp).zp)
+#define CCDH_GP_T_GP(gp) ((gp).gp)
+
+#define CCDH_CONST_GP_T_ZP(gp) ((gp).zp)
+#define CCDH_CONST_GP_T_GP(gp) ((gp).gp)
+#else
+typedef ccdh_gp* ccdh_gp_t;
+typedef const ccdh_gp* ccdh_const_gp_t;
+
+#define CCDH_GP_T_ZP(gp) ((cczp_t)(gp))
+#define CCDH_GP_T_GP(gp) ((ccdh_gp*)(gp))
+
+#define CCDH_CONST_GP_T_ZP(gp) ((cczp_const_t)(gp))
+#define CCDH_CONST_GP_T_GP(gp) ((const ccdh_gp*)(gp))
+#endif
 
 struct ccdh_ctx_header {
     ccdh_const_gp_t     gp;
@@ -47,6 +64,7 @@ typedef struct ccdh_full_ctx {
     struct ccdh_ctx_header  hdr;
 } __attribute__((aligned(16))) ccdh_full_ctx;
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     ccdh_full_ctx *_full;
     struct ccdh_ctx_header *hdr;
@@ -54,10 +72,22 @@ typedef union {
     struct ccdh_ctx_public *pub;
 } __attribute__((transparent_union)) ccdh_full_ctx_t;
 
+#define CCDH_FULL_CTX_T_HDR(ctx) ((ctx).hdr)
+#define CCDH_FULL_CTX_T_BODY(ctx) ((ctx).body)
+#define CCDH_FULL_CTX_T_PUB(ctx) ((ctx).pub)
+#else
+typedef ccdh_full_ctx* ccdh_full_ctx_t;
+
+#define CCDH_FULL_CTX_T_HDR(ctx) ((struct ccdh_ctx_header*)(ctx))
+#define CCDH_FULL_CTX_T_BODY(ctx) ((struct ccdh_ctx_body*)(ctx))
+#define CCDH_FULL_CTX_T_PUB(ctx) ((struct ccdh_ctx_public*)(ctx))
+#endif
+
 typedef struct ccdh_pub_ctx {
     struct ccdh_ctx_header  hdr;
 } __attribute__((aligned(16))) ccdh_pub_ctx;
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     ccdh_pub_ctx *_pub;
     ccdh_full_ctx *_full;
@@ -66,6 +96,19 @@ typedef union {
     struct ccdh_ctx_public *pub;
     ccdh_full_ctx_t fullt;
 } __attribute__((transparent_union)) ccdh_pub_ctx_t;
+
+#define CCDH_PUB_CTX_T_HDR(ctx) ((ctx).hdr)
+#define CCDH_PUB_CTX_T_BODY(ctx) ((ctx).body)
+#define CCDH_PUB_CTX_T_PUB(ctx) ((ctx).pub)
+#define CCDH_PUB_CTX_T_FULLT(ctx) ((ctx).fullt)
+#else
+typedef ccdh_pub_ctx* ccdh_pub_ctx_t;
+
+#define CCDH_PUB_CTX_T_HDR(ctx) ((struct ccdh_ctx_header*)(ctx))
+#define CCDH_PUB_CTX_T_BODY(ctx) ((struct ccdh_ctx_body*)(ctx))
+#define CCDH_PUB_CTX_T_PUB(ctx) ((struct ccdh_ctx_public*)(ctx))
+#define CCDH_PUB_CTX_T_FULLT(ctx) ((ccdh_full_ctx_t)(ctx))
+#endif
 
 // Functions
 CC_NONNULL_TU((1,2)) CC_NONNULL3
@@ -78,7 +121,7 @@ int ccdh_import_pub(ccdh_const_gp_t gp, size_t in_len, const uint8_t *in,
 
 CC_CONST CC_INLINE CC_NONNULL_TU((1))
 cc_size ccdh_gp_n(ccdh_const_gp_t gp) {
-    return cczp_n(gp.zp);
+    return cczp_n(CCDH_CONST_GP_T_ZP(gp));
 }
 
 CC_NONNULL_TU((1)) CC_NONNULL2
@@ -87,14 +130,14 @@ int ccdh_generate_key(ccdh_const_gp_t gp, struct ccrng_state *rng,
 
 CC_CONST CC_INLINE CC_NONNULL_TU((1))
 size_t ccdh_ccn_size(ccdh_const_gp_t gp) {
-    return ccn_sizeof_n(cczp_n(gp.zp));
+    return ccn_sizeof_n(cczp_n(CCDH_CONST_GP_T_ZP(gp)));
 }
 
 CC_NONNULL_TU((1)) CC_NONNULL2
 void ccdh_export_pub(ccdh_pub_ctx_t key, void *out);
 
 #define ccdh_gp_prime_size(GP)  (ccdh_ccn_size(GP))
-#define ccdh_ctx_gp(KEY)     (((ccdh_pub_ctx_t)(KEY)).hdr->gp)
+#define ccdh_ctx_gp(KEY)     (CCDH_PUB_CTX_T_HDR((ccdh_pub_ctx_t)(KEY))->gp)
 CC_INLINE CC_CONST CC_NONNULL_TU((1))
 size_t ccdh_export_pub_size(ccdh_pub_ctx_t key) {
     return ccdh_gp_prime_size(ccdh_ctx_gp(key));
@@ -107,21 +150,22 @@ size_t ccdh_export_pub_size(ccdh_pub_ctx_t key) {
 
 #define ccdh_pub_ctx_decl_gp(_gp_, _name_)  ccdh_pub_ctx_decl(ccdh_ccn_size(_gp_), _name_)
 
-#define ccdh_ctx_y(KEY)  (((ccdh_pub_ctx_t)(KEY)).body->ccn)
+#define ccdh_ctx_y(KEY)  (CCDH_PUB_CTX_T_BODY((ccdh_pub_ctx_t)(KEY))->ccn)
 
 #define ccdh_full_ctx_size(_size_)  (sizeof(struct ccdh_ctx_header) + 2 * (_size_))
 
 #define ccdh_gp_size(_size_) (cczp_size(_size_) + 2 * (_size_) + ccn_sizeof_n(1))
 
-#define CCDH_GP_PRIME(_gp_)     (CCZP_PRIME((_gp_).zp))
+#define CCDH_GP_PRIME(_gp_)     (CCZP_PRIME(CCDH_GP_T_ZP(_gp_)))
 
-#define CCDH_GP_N(_gp_)         (CCZP_N((_gp_).zp))
+#define CCDH_GP_N(_gp_)         (CCZP_N(CCDH_GP_T_ZP(_gp_)))
 
-#define CCDH_GP_RECIP(_gp_)     (CCZP_RECIP((_gp_).zp))
+#define CCDH_GP_RECIP(_gp_)     (CCZP_RECIP(CCDH_GP_T_ZP(_gp_)))
 #define CCDH_GP_G(_gp_)         (CCDH_GP_RECIP(_gp_) + 1 + ccdh_gp_n(_gp_))
-#define CCDH_GP_N(_gp_)         (CCZP_N((_gp_).zp))
+#define CCDH_GP_N(_gp_)         (CCZP_N(CCDH_GP_T_ZP(_gp_)))
 #define CCDH_GP_Q(_gp_)         (CCDH_GP_G(_gp_) + ccdh_gp_n(_gp_))
 #define CCDH_GP_L(_gp_)         (*(cc_size *)((cc_unit *)(CCDH_GP_Q(_gp_) + ccdh_gp_n(_gp_))))
+#define CCDH_GP_ZP(_gp_)        (CCDH_GP_T_ZP(_gp_))
 
 #define ccdh_pub_ctx_size(_size_)   (sizeof(struct ccdh_ctx_header) + 1 * (_size_))
 
@@ -129,14 +173,20 @@ size_t ccdh_export_pub_size(ccdh_pub_ctx_t key) {
 
 #define ccdh_ctx_n(KEY)      (ccdh_gp_n(ccdh_ctx_gp(KEY)))
 
+#define ccdh_ctx_public(KEY) ((ccdh_pub_ctx_t)(KEY))
+
 CC_CONST CC_INLINE CC_NONNULL_TU((1))
 const cc_unit *ccdh_gp_g(ccdh_const_gp_t gp) {
+#if CORECRYPTO_USE_TRANSPARENT_UNION
     return CCDH_GP_G(gp._ncgp);
+#else
+    return CCDH_GP_G((ccdh_gp_t)gp);
+#endif
 }
 
 CC_CONST CC_INLINE CC_NONNULL_TU((1))
 const cc_unit *ccdh_gp_prime(ccdh_const_gp_t gp) {
-    return cczp_prime(gp.zp);
+    return cczp_prime(CCDH_CONST_GP_T_ZP(gp));
 }
 
 CC_NONNULL((1))
@@ -147,7 +197,7 @@ const uint8_t *ccder_decode_dhparams(ccdh_gp_t gp, const uint8_t *der, const uin
 
 CC_INLINE CC_NONNULL_TU((1))
 void ccdh_ctx_init(ccdh_const_gp_t gp, ccdh_pub_ctx_t key) {
-    key.hdr->gp = gp;
+    CCDH_PUB_CTX_T_HDR(key)->gp = gp;
 }
 
 CC_NONNULL_TU((1)) CC_NONNULL((3, 4))

include/corecrypto/ccdigest.h
@@ -26,9 +26,17 @@ struct ccdigest_ctx {
     } state;
 } CC_ALIGNED(8);
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     struct ccdigest_ctx *hdr;
 } ccdigest_ctx_t __attribute__((transparent_union));
+
+#define CCDIGEST_CTX_T_HDR(ctx) ((ctx).hdr)
+#else
+typedef struct ccdigest_ctx* ccdigest_ctx_t;
+
+#define CCDIGEST_CTX_T_HDR(ctx) ((struct ccdigest_ctx*)(ctx))
+#endif
 
 struct ccdigest_state {
     union {
@@ -100,7 +108,7 @@ struct ccdigest_info {
 
 /* Digest context field accessors.  Consider the implementation private. */
 #if CORECRYPTO_USE_TRANSPARENT_UNION
-#define ccdigest_state(_di_, _ctx_)      ((struct ccdigest_state *)(&((ccdigest_ctx_t)(_ctx_)).hdr->state.u8 + sizeof(uint64_t)))
+#define ccdigest_state(_di_, _ctx_)      ((struct ccdigest_state *)(&CCDIGEST_CTX_T_HDR((ccdigest_ctx_t)(_ctx_))->state.u8 + sizeof(uint64_t)))
 #else
 #define ccdigest_state(_di_, _ctx_)      ((struct ccdigest_state *)(&((ccdigest_ctx_t)(_ctx_))->state.u8 + sizeof(uint64_t)))
 #endif
@@ -111,9 +119,9 @@ struct ccdigest_info {
 #define ccdigest_state_ccn(_di_, _ctx_)  ccdigest_ccn(ccdigest_state((_di_), (_ctx_)))
 
 #if CORECRYPTO_USE_TRANSPARENT_UNION
-#define ccdigest_nbits(_di_, _ctx_)      (((uint64_t *)(&((ccdigest_ctx_t)(_ctx_)).hdr->state.u8))[0])
-#define ccdigest_data(_di_, _ctx_)       (&((ccdigest_ctx_t)(_ctx_)).hdr->state.u8 + (_di_)->state_size + sizeof(uint64_t))
-#define ccdigest_num(_di_, _ctx_)        (((unsigned int *)(&((ccdigest_ctx_t)(_ctx_)).hdr->state.u8 + (_di_)->state_size + sizeof(uint64_t) + (_di_)->block_size))[0])
+#define ccdigest_nbits(_di_, _ctx_)      (((uint64_t *)(&CCDIGEST_CTX_T_HDR((ccdigest_ctx_t)(_ctx_))->state.u8))[0])
+#define ccdigest_data(_di_, _ctx_)       (&CCDIGEST_CTX_T_HDR((ccdigest_ctx_t)(_ctx_))->state.u8 + (_di_)->state_size + sizeof(uint64_t))
+#define ccdigest_num(_di_, _ctx_)        (((unsigned int *)(&CCDIGEST_CTX_T_HDR((ccdigest_ctx_t)(_ctx_))->state.u8 + (_di_)->state_size + sizeof(uint64_t) + (_di_)->block_size))[0])
 #else
 #define ccdigest_nbits(_di_, _ctx_)      (((uint64_t *)(&((ccdigest_ctx_t)(_ctx_))->state.u8))[0])
 #define ccdigest_data(_di_, _ctx_)       (&((ccdigest_ctx_t)(_ctx_))->state.u8 + (_di_)->state_size + sizeof(uint64_t))
@@ -122,10 +130,10 @@ struct ccdigest_info {
 
 #if CORECRYPTO_USE_TRANSPARENT_UNION
 /* Digest state field accessors.  Consider the implementation private. */
-#define ccdigest_u8(_state_)             (&((ccdigest_state_t)(_state_)).hdr->state.u8)
-#define ccdigest_u32(_state_)            (&((ccdigest_state_t)(_state_)).hdr->state.u32)
-#define ccdigest_u64(_state_)            (&((ccdigest_state_t)(_state_)).hdr->state.u64)
-#define ccdigest_ccn(_state_)            (&((ccdigest_state_t)(_state_)).hdr->state.ccn)
+#define ccdigest_u8(_state_)             (&CCDIGEST_CTX_T_HDR((ccdigest_state_t)(_state_))->state.u8)
+#define ccdigest_u32(_state_)            (&CCDIGEST_CTX_T_HDR((ccdigest_state_t)(_state_))->state.u32)
+#define ccdigest_u64(_state_)            (&CCDIGEST_CTX_T_HDR((ccdigest_state_t)(_state_))->state.u64)
+#define ccdigest_ccn(_state_)            (&CCDIGEST_CTX_T_HDR((ccdigest_state_t)(_state_))->state.ccn)
 #else
 /* Digest state field accessors.  Consider the implementation private. */
 #define ccdigest_u8(_state_)             (&((ccdigest_state_t)(_state_))->state.u8)

include/corecrypto/cchmac.h
@@ -19,10 +19,20 @@ struct cchmac_ctx {
     uint8_t b[8];
 } CC_ALIGNED(8);
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     struct cchmac_ctx *hdr;
     ccdigest_ctx_t digest;
 } cchmac_ctx_t __attribute__((transparent_union));
+
+#define CCHMAC_CTX_T_HDR(ctx) ((ctx).hdr)
+#define CCHMAC_CTX_T_DIGEST(ctx) ((ctx).digest)
+#else
+typedef struct cchmac_ctx* cchmac_ctx_t;
+
+#define CCHMAC_CTX_T_HDR(ctx) ((struct cchmac_ctx*)(ctx))
+#define CCHMAC_CTX_T_DIGEST(ctx) ((ccdigest_ctx_t)(ctx))
+#endif
 
 #define cchmac_ctx_size(STATE_SIZE, BLOCK_SIZE)  (ccdigest_ctx_size(STATE_SIZE, BLOCK_SIZE) + (STATE_SIZE))
 #define cchmac_di_size(_di_)  (cchmac_ctx_size((_di_)->state_size, (_di_)->block_size))
@@ -35,24 +45,24 @@ typedef union {
 #define cchmac_di_clear(_di_, _name_) cchmac_ctx_clear((_di_)->state_size, (_di_)->block_size, _name_)
 
 /* Return a ccdigest_ctx_t which can be accesed with the macros in ccdigest.h */
-#define cchmac_digest_ctx(_di_, HC)    (((cchmac_ctx_t)(HC)).digest)
+#define cchmac_digest_ctx(_di_, HC)    (CCHMAC_CTX_T_DIGEST((cchmac_ctx_t)(HC)))
 
 /* Accesors for ostate fields, this is all cchmac_ctx_t adds to the ccdigest_ctx_t. */
-#define cchmac_ostate(_di_, HC)    ((struct ccdigest_state *)(((cchmac_ctx_t)(HC)).hdr->b + ccdigest_di_size(_di_)))
+#define cchmac_ostate(_di_, HC)    ((struct ccdigest_state *)(CCHMAC_CTX_T_HDR((cchmac_ctx_t)(HC))->b + ccdigest_di_size(_di_)))
 #define cchmac_ostate8(_di_, HC)   (ccdigest_u8(cchmac_ostate(_di_, HC)))
 #define cchmac_ostate32(_di_, HC)  (ccdigest_u32(cchmac_ostate(_di_, HC)))
 #define cchmac_ostate64(_di_, HC)  (ccdigest_u64(cchmac_ostate(_di_, HC)))
 #define cchmac_ostateccn(_di_, HC) (ccdigest_ccn(cchmac_ostate(_di_, HC)))
 
 /* Convenience accessors for ccdigest_ctx_t fields. */
-#define cchmac_istate(_di_, HC)    ccdigest_state(_di_, ((cchmac_ctx_t)(HC)).digest)
+#define cchmac_istate(_di_, HC)    ccdigest_state(_di_, CCHMAC_CTX_T_DIGEST((cchmac_ctx_t)(HC)))
 #define cchmac_istate8(_di_, HC)   ccdigest_u8(cchmac_istate(_di_, HC))
 #define cchmac_istate32(_di_, HC)  ccdigest_u32(cchmac_istate(_di_, HC))
 #define cchmac_istate64(_di_, HC)  ccdigest_u64(cchmac_istate(_di_, HC))
 #define cchmac_istateccn(_di_, HC) ccdigest_ccn(cchmac_istate(_di_, HC))
-#define cchmac_data(_di_, HC)      ccdigest_data(_di_, ((cchmac_ctx_t)(HC)).digest)
-#define cchmac_num(_di_, HC)       ccdigest_num(_di_, ((cchmac_ctx_t)(HC)).digest)
-#define cchmac_nbits(_di_, HC)     ccdigest_nbits(_di_, ((cchmac_ctx_t)(HC)).digest)
+#define cchmac_data(_di_, HC)      ccdigest_data(_di_, CCHMAC_CTX_T_DIGEST((cchmac_ctx_t)(HC)))
+#define cchmac_num(_di_, HC)       ccdigest_num(_di_, CCHMAC_CTX_T_DIGEST((cchmac_ctx_t)(HC)))
+#define cchmac_nbits(_di_, HC)     ccdigest_nbits(_di_, CCHMAC_CTX_T_DIGEST((cchmac_ctx_t)(HC)))
 
 void cchmac_init(const struct ccdigest_info *di, cchmac_ctx_t ctx,
                  size_t key_len, const void *key);

include/corecrypto/ccrsa.h
@@ -13,6 +13,7 @@ cc_aligned_struct(16) ccrsa_full_ctx;
 cc_aligned_struct(16) ccrsa_priv_ctx;
 cc_aligned_struct(16) ccrsa_pub_ctx;
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     cczp_t zp;
     ccrsa_priv_ctx *priv;
@@ -30,21 +31,48 @@ typedef union {
     ccrsa_full_ctx_t full_t;
 } ccrsa_pub_ctx_t __attribute__((transparent_union));
 
+#define CCRSA_PRIV_CTX_T_ZP(ctx) ((ctx).zp)
+#define CCRSA_PRIV_CTX_T_PRIV(ctx) ((ctx).priv)
+
+#define CCRSA_FULL_CTX_T_ZP(ctx) ((ctx).zp)
+#define CCRSA_FULL_CTX_T_FULL(ctx) ((ctx).full)
+
+#define CCRSA_PUB_CTX_T_ZP(ctx) ((ctx).zp)
+#define CCRSA_PUB_CTX_T_PUB(ctx) ((ctx).pub)
+#define CCRSA_PUB_CTX_T_FULL(ctx) ((ctx).full)
+#define CCRSA_PUB_CTX_T_FULL_T(ctx) ((ctx).full_t)
+#else
+typedef ccrsa_priv_ctx* ccrsa_priv_ctx_t;
+typedef ccrsa_full_ctx* ccrsa_full_ctx_t;
+typedef ccrsa_pub_ctx* ccrsa_pub_ctx_t;
+
+#define CCRSA_PRIV_CTX_T_ZP(ctx) ((cczp_t)(ctx))
+#define CCRSA_PRIV_CTX_T_PRIV(ctx) ((ccrsa_priv_ctx*)(ctx))
+
+#define CCRSA_FULL_CTX_T_ZP(ctx) ((cczp_t)(ctx))
+#define CCRSA_FULL_CTX_T_FULL(ctx) ((ccrsa_full_ctx*)(ctx))
+
+#define CCRSA_PUB_CTX_T_ZP(ctx) ((cczp_t)(ctx))
+#define CCRSA_PUB_CTX_T_PUB(ctx) ((ccrsa_pub_ctx*)(ctx))
+#define CCRSA_PUB_CTX_T_FULL(ctx) ((ccrsa_full_ctx*)(ctx))
+#define CCRSA_PUB_CTX_T_FULL_T(ctx) ((ccrsa_full_ctx_t)(ctx))
+#endif
+
 // Macros
 
-#define ccrsa_ctx_zm(_ctx_)        (((ccrsa_pub_ctx_t)(_ctx_)).zp)
-#define ccrsa_ctx_n(_ctx_)         (ccrsa_ctx_zm(_ctx_).zp->n)
-#define ccrsa_ctx_m(_ctx_)         (ccrsa_ctx_zm(_ctx_).prime->ccn)
+#define ccrsa_ctx_zm(_ctx_)        (CCRSA_PUB_CTX_T_ZP((ccrsa_pub_ctx_t)(_ctx_)))
+#define ccrsa_ctx_n(_ctx_)         (CCZP_T_ZP(ccrsa_ctx_zm(_ctx_))->n)
+#define ccrsa_ctx_m(_ctx_)         (CCZP_T_PRIME(ccrsa_ctx_zm(_ctx_))->ccn)
 #define ccrsa_ctx_e(_ctx_)         (ccrsa_ctx_m(_ctx_) + 2 * ccrsa_ctx_n(_ctx_) + 1)
 #define ccrsa_ctx_d(_ctx_)         (ccrsa_ctx_m(((ccrsa_full_ctx_t)_ctx_)) + 3 * ccrsa_ctx_n(_ctx_) + 1)
 
-#define ccrsa_ctx_private_zq(PRIVK)   ((cczp_t)(((ccrsa_priv_ctx_t)(PRIVK)).zp.prime->ccn + 2 * ccrsa_ctx_private_zp(PRIVK).zp->n + 1))
+#define ccrsa_ctx_private_zq(PRIVK)   ((cczp_t)(CCZP_T_PRIME(CCRSA_PRIV_CTX_T_ZP((ccrsa_priv_ctx_t)(PRIVK)))->ccn + 2 * CCZP_T_ZP(ccrsa_ctx_private_zp(PRIVK))->n + 1))
 #define ccrsa_pub_ctx_size(_size_)   (sizeof(struct cczp) + CCN_UNIT_SIZE + 3 * (_size_))
-#define ccrsa_ctx_private_zp(PRIVK)   (((ccrsa_priv_ctx_t)(PRIVK)).zp)
-#define ccrsa_ctx_private_dq(PRIVK)   (((ccrsa_priv_ctx_t)(PRIVK)).zp.prime->ccn + 5 * ccrsa_ctx_private_zp(PRIVK).zp->n + 2 + ccn_nof_size(sizeof(struct cczp)))
-#define ccrsa_ctx_private_qinv(PRIVK) (((ccrsa_priv_ctx_t)(PRIVK)).zp.prime->ccn + 6 * ccrsa_ctx_private_zp(PRIVK).zp->n + 2 + ccn_nof_size(sizeof(struct cczp)))
+#define ccrsa_ctx_private_zp(PRIVK)   (CCRSA_PRIV_CTX_T_ZP((ccrsa_priv_ctx_t)(PRIVK)))
+#define ccrsa_ctx_private_dq(PRIVK)   (CCZP_T_PRIME(CCRSA_PRIV_CTX_T_ZP((ccrsa_priv_ctx_t)(PRIVK)))->ccn + 5 * CCZP_T_ZP(ccrsa_ctx_private_zp(PRIVK))->n + 2 + ccn_nof_size(sizeof(struct cczp)))
+#define ccrsa_ctx_private_qinv(PRIVK) (CCZP_T_PRIME(CCRSA_PRIV_CTX_T_ZP((ccrsa_priv_ctx_t)(PRIVK)))->ccn + 6 * CCZP_T_ZP(ccrsa_ctx_private_zp(PRIVK))->n + 2 + ccn_nof_size(sizeof(struct cczp)))
 
-#define ccrsa_ctx_private_dp(PRIVK)   (((ccrsa_priv_ctx_t)(PRIVK)).zp.prime->ccn + 4 * ccrsa_ctx_private_zp(PRIVK).zp->n + 2 + ccn_nof_size(sizeof(struct cczp)))
+#define ccrsa_ctx_private_dp(PRIVK)   (CCZP_T_PRIME(CCRSA_PRIV_CTX_T_ZP((ccrsa_priv_ctx_t)(PRIVK)))->ccn + 4 * CCZP_T_ZP(ccrsa_ctx_private_zp(PRIVK))->n + 2 + ccn_nof_size(sizeof(struct cczp)))
 
 #define ccrsa_priv_ctx_size(_size_)  ((sizeof(struct cczp) + CCN_UNIT_SIZE) * 2 + 7 * ccn_sizeof((ccn_bitsof_size(_size_) / 2) + 1))
 
@@ -61,7 +89,7 @@ typedef union {
 
 CC_CONST CC_INLINE
 ccrsa_priv_ctx_t ccrsa_ctx_private(ccrsa_full_ctx_t fk) {
-    uint8_t *p = (uint8_t *)fk.full;
+    uint8_t *p = (uint8_t *)CCRSA_FULL_CTX_T_FULL(fk);
     size_t p_size = ccn_sizeof_n(ccrsa_ctx_n(fk));
     p += ccrsa_pub_ctx_size(p_size) + p_size;
     ccrsa_priv_ctx *priv = (ccrsa_priv_ctx *)p;

include/corecrypto/ccsrp.h
@@ -34,11 +34,21 @@ struct ccsrp_ctx_body {
     cc_unit              ccn[];
 } __attribute__((aligned(16)));
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     ccsrp_ctx *_full;
     struct ccsrp_ctx_header *hdr;
     struct ccsrp_ctx_body *body;
 } __attribute__((transparent_union)) ccsrp_ctx_t;
+
+#define CCSRP_CTX_T_HDR(ctx) ((ctx).hdr)
+#define CCSRP_CTX_T_BODY(ctx) ((ctx).body)
+#else
+typedef ccsrp_ctx* ccsrp_ctx_t;
+
+#define CCSRP_CTX_T_HDR(ctx) ((struct ccsrp_ctx_header*)(ctx))
+#define CCSRP_CTX_T_BODY(ctx) ((struct ccsrp_ctx_body*)(ctx))
+#endif
 
 #define ccsrp_gpbuf_size(_gp_) (ccdh_ccn_size(_gp_)*3)
 #define ccsrp_dibuf_size(_di_) ((_di_)->output_size*3)
@@ -49,14 +59,14 @@ typedef union {
 #define ccsrp_ctx_decl(_di_, _gp_, _name_) \
     cc_ctx_decl(ccsrp_ctx, ccsrp_sizeof_srp(_di_,_gp_), _name_)
 
-#define ccsrp_ctx_gp(KEY)   (((ccsrp_ctx_t)(KEY)).hdr->gp)
-#define ccsrp_ctx_di(KEY)   (((ccsrp_ctx_t)(KEY)).hdr->di)
-#define ccsrp_ctx_zp(KEY)   (ccsrp_ctx_gp(KEY).zp)
+#define ccsrp_ctx_gp(KEY)   (CCSRP_CTX_T_HDR((ccsrp_ctx_t)(KEY))->gp)
+#define ccsrp_ctx_di(KEY)   (CCSRP_CTX_T_HDR((ccsrp_ctx_t)(KEY))->di)
+#define ccsrp_ctx_zp(KEY)   (CCDH_CONST_GP_T_ZP(ccsrp_ctx_gp(KEY)))
 #define ccsrp_ctx_gp_g(KEY)   (ccdh_gp_g(ccsrp_ctx_gp(KEY)))
 #define ccsrp_ctx_gp_l(KEY)   (ccdh_gp_l(ccsrp_ctx_gp(KEY)))
 #define ccsrp_ctx_n(KEY)      (ccdh_gp_n(ccsrp_ctx_gp(KEY)))
 #define ccsrp_ctx_prime(KEY)  (ccdh_gp_prime(ccsrp_ctx_gp(KEY)))
-#define ccsrp_ctx_ccn(KEY)  (((ccsrp_ctx_t)(KEY)).hdr->ccn)
+#define ccsrp_ctx_ccn(KEY)  (CCSRP_CTX_T_HDR((ccsrp_ctx_t)(KEY))->ccn)
 #define ccsrp_ctx_pki_key(KEY,_N_) (ccsrp_ctx_ccn(KEY) + ccsrp_ctx_n(KEY) * _N_)
 #define ccsrp_ctx_public(KEY)  (ccsrp_ctx_pki_key(KEY,0))
 #define ccsrp_ctx_private(KEY)  (ccsrp_ctx_pki_key(KEY,1))
@@ -73,10 +83,10 @@ typedef union {
 
 CC_INLINE void
 ccsrp_ctx_init(ccsrp_ctx_t srp, const struct ccdigest_info *di, ccsrp_const_gp_t gp) {
-    cc_zero(ccsrp_sizeof_srp(di, gp),srp.hdr);
-    srp.hdr->di = di;
-    srp.hdr->gp = gp;
-    srp.hdr->flags.authenticated = false;
+    cc_zero(ccsrp_sizeof_srp(di, gp), CCSRP_CTX_T_HDR(srp));
+    CCSRP_CTX_T_HDR(srp)->di = di;
+    CCSRP_CTX_T_HDR(srp)->gp = gp;
+    CCSRP_CTX_T_HDR(srp)->flags.authenticated = false;
 }
 
 #define CCSRP_ERROR_DEFAULT                 CCDH_ERROR_DEFAULT
@@ -139,12 +149,12 @@ ccsrp_client_verify_session(ccsrp_ctx_t srp, const uint8_t *HAMK_bytes);
 CC_INLINE bool
 ccsrp_client_set_noUsernameInX(ccsrp_ctx_t srp, bool flag)
 {
-    return srp.hdr->flags.noUsernameInX = !!flag;
+    return CCSRP_CTX_T_HDR(srp)->flags.noUsernameInX = !!flag;
 }
 
 CC_INLINE bool
 ccsrp_is_authenticated(ccsrp_ctx_t srp) {
-	return srp.hdr->flags.authenticated;
+	return CCSRP_CTX_T_HDR(srp)->flags.authenticated;
 }
 
 

include/corecrypto/cczp.h
@@ -21,6 +21,7 @@ struct cczp_prime {
     cc_unit ccn[];
 };
 
+#if CORECRYPTO_USE_TRANSPARENT_UNION
 typedef union {
     cc_unit *u;
     struct cczp *zp;
@@ -52,37 +53,79 @@ typedef union {
     cczp_t _lzp;
 } cczp_const_short_t __attribute__((transparent_union));
 
+#define CCZP_T_U(_cczp_) ((_cczp_).u)
+#define CCZP_T_ZP(_cczp_) ((_cczp_).zp)
+#define CCZP_T_PRIME(_cczp_) ((_cczp_).prime)
+
+#define CCZP_SHORT_T_U(_cczp_) ((_cczp_).u)
+#define CCZP_SHORT_T_ZP(_cczp_) ((_cczp_).zp)
+#define CCZP_SHORT_T_PRIME(_cczp_) ((_cczp_).prime)
+#define CCZP_SHORT_T_SZP(_cczp_) ((_cczp_).szp)
+
+#define CCZP_CONST_T_U(_cczp_) ((_cczp_).u)
+#define CCZP_CONST_T_ZP(_cczp_) ((_cczp_).zp)
+#define CCZP_CONST_T_PRIME(_cczp_) ((_cczp_).prime)
+
+#define CCZP_CONST_SHORT_T_U(_cczp_) ((_cczp_).u)
+#define CCZP_CONST_SHORT_T_ZP(_cczp_) ((_cczp_).zp)
+#define CCZP_CONST_SHORT_T_PRIME(_cczp_) ((_cczp_).prime)
+#define CCZP_CONST_SHORT_T_SZP(_cczp_) ((_cczp_).szp)
+#else
+typedef struct cczp* cczp_t;
+typedef struct cczp_short* cczp_short_t;
+typedef const struct cczp* cczp_const_t;
+typedef const struct cczp_short* cczp_const_short_t;
+
+#define CCZP_T_U(_cczp_) ((cc_unit*)(_cczp_))
+#define CCZP_T_ZP(_cczp_) ((struct cczp*)(_cczp_))
+#define CCZP_T_PRIME(_cczp_) ((struct cczp_prime*)(_cczp_))
+
+#define CCZP_SHORT_T_U(_cczp_) ((cc_unit*)(_cczp_))
+#define CCZP_SHORT_T_ZP(_cczp_) ((struct cczp*)(_cczp_))
+#define CCZP_SHORT_T_PRIME(_cczp_) ((struct cczp_prime*)(_cczp_))
+#define CCZP_SHORT_T_SZP(_cczp_) (((struct cczp_short*)_cczp_))
+
+#define CCZP_CONST_T_U(_cczp_) ((const cc_unit*)(_cczp_))
+#define CCZP_CONST_T_ZP(_cczp_) ((const struct cczp*)(_cczp_))
+#define CCZP_CONST_T_PRIME(_cczp_) ((const struct cczp_prime*)(_cczp_))
+
+#define CCZP_CONST_SHORT_T_U(_cczp_) ((const cc_unit*)(_cczp_))
+#define CCZP_CONST_SHORT_T_ZP(_cczp_) ((const struct cczp*)(_cczp_))
+#define CCZP_CONST_SHORT_T_PRIME(_cczp_) ((const struct cczp_prime*)(_cczp_))
+#define CCZP_CONST_SHORT_T_SZP(_cczp_) ((struct cczp_short*)(_cczp_))
+#endif
+
 typedef void(ccmod_prime_f)(cczp_const_t, cc_unit *, const cc_unit *, cc_ws_t);
 
-#define CCZP_PRIME(ZP) (((cczp_short_t)(ZP)).prime->ccn)
+#define CCZP_PRIME(ZP) (CCZP_SHORT_T_PRIME((cczp_short_t)(ZP))->ccn)
 
 // Macros
 
-#define CCZP_RECIP(ZP) (((cczp_t)(ZP)).prime->ccn + cczp_n(ZP))
+#define CCZP_RECIP(ZP) (CCZP_T_PRIME((cczp_t)(ZP))->ccn + cczp_n(ZP))
 #define cczp_size(_size_) (sizeof(struct cczp) + ccn_sizeof_n(1) + 2 * (_size_))
-#define CCZP_N(ZP) (((cczp_short_t)(ZP)).zp->n)
-#define CCZP_MOD_PRIME(ZP) (((cczp_short_t)(ZP)).zp->mod_prime)
+#define CCZP_N(ZP) (CCZP_SHORT_T_ZP((cczp_short_t)(ZP))->n)
+#define CCZP_MOD_PRIME(ZP) (CCZP_SHORT_T_ZP((cczp_short_t)(ZP))->mod_prime)
 
 // Functions
 
 CC_CONST CC_INLINE cc_size cczp_n(cczp_const_short_t zp)
 {
-	return zp.zp->n;
+	return CCZP_CONST_SHORT_T_ZP(zp)->n;
 }
 
 CC_CONST CC_INLINE const cc_unit *cczp_recip(cczp_const_t zp)
 {
-	return zp.u + cczp_n(zp) + ccn_nof_size(sizeof(struct cczp));
+	return CCZP_CONST_T_U(zp) + cczp_n(zp) + ccn_nof_size(sizeof(struct cczp));
 }
 
 CC_CONST CC_INLINE const cc_unit *cczp_prime(cczp_const_short_t zp)
 {
-	return zp.u + ccn_nof_size(sizeof(struct cczp));
+	return CCZP_CONST_SHORT_T_U(zp) + ccn_nof_size(sizeof(struct cczp));
 }
 
 CC_CONST CC_INLINE ccmod_prime_f *cczp_mod_prime(cczp_const_t zp)
 {
-	return zp.zp->mod_prime;
+	return CCZP_CONST_T_ZP(zp)->mod_prime;
 }
 
 void cczp_init(cczp_t zp);

commit 9752b15651e08d9b3da48bc5f9f1509d61222ff8

Manually Undo Additional Changes Caused By c3013bb

include/corecrypto/ccec.h
@@ -117,7 +117,7 @@ size_t ccec_sign_max_size(ccec_const_cp_t cp) {
 
 CC_INLINE CC_NONNULL_TU((1, 2))
 void ccec_pub_ctx_clear_cp(ccec_const_cp_t cp, ccec_pub_ctx_t ctx) {
-	cc_clear(ccec_pub_ctx_size(ccec_ccn_size(cp)) - sizeof(struct ccec_ctx_header), CCEC_PUB_CTX_PUB(ctx)->point);
+	cc_clear(ccec_pub_ctx_size(ccec_ccn_size(cp)) - sizeof(struct ccec_ctx_header), ctx.pub->point);
 };
 
 ccec_const_cp_t ccec_get_cp(size_t keysize);